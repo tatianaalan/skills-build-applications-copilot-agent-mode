e010b6b0fdccde1b6010f2d2fb354e9080777b15
tatianaalan <tatianaalanli@gmail.com>
2026-06-17 14:56:30 -0400

    Add React App component

---
+++ octofit-tracker/frontend/src/App.jsx
@@ -0,0 +1,10 @@
+import { useState } from 'react'
+
+export default function App() {
+  return (
+    <div>
+      <h1>OctoFit Tracker</h1>
+      <p>Welcome to your fitness tracking application</p>
+    </div>
+  )
+}
